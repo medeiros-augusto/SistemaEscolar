e48364a845f523e866debd4148a333e4655a3d14
medeiros-augusto <augustomedeiros047@gmail.com>
2025-01-11 18:39:03 -0300

    commit gisele

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const port = 3010;
 var path = require('path')
 const app = express()
 
+app.use(session({ secret: 'gsdgsdfqfq2f3fd' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use('/logspdf', express.static(path.join(__dirname, 'logspdf')));
+app.set('views', path.join(__dirname, '/views'))
+
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
