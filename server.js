7cc13602f8ed3f12c64a6ecafd50d8ba8d40bea2
medeiros-augusto <augustomedeiros047@gmail.com>
2025-01-11 18:51:05 -0300

    image e server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session')
 const bodyParser = require('body-parser')
 const mysql = require('mysql2')
 
-const port = 3010;
+const port = 3000;
 var path = require('path')
 const app = express()
 
@@ -13,8 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 app.use('/public', express.static(path.join(__dirname, 'public')))
-app.use('/logspdf', express.static(path.join(__dirname, 'logspdf')));
-app.set('views', path.join(__dirname, '/views'))
+app.set('/views', path.join(__dirname, '/views'))
 
 const connection = mysql.createConnection({
     host: '127.0.0.1',
@@ -32,7 +31,35 @@ connection.connect(function (err) {
 });
 
 app.get('/', (req, res) => {
-    res.send("Deu boa")
+        res.render('index')
+    
+})
+app.post('/', (req, res) => {
+    global.nomelogin = req.body.nomelogin
+    let senha = req.body.senhalogin
+
+    connection.query("SELECT * FROM usuario where nome_usuario = '" + global.nomelogin + "'", function (err, rows, fields) {
+        if (!err) {
+            if (rows.length > 0) {
+                if (rows[0].senha_usuario === senha) {
+                    req.session.nomelogin = global.nomelogin
+                    req.session.idlogin = rows[0].id_usuario
+                    if (req.session.nomelogin == 'adm') {
+                        res.render('painel_adm', { login: global.nomelogin })
+                    } else {
+                        res.render('home', { login: global.nomelogin })
+                    }
+                } else {
+                    res.render('login')
+                }
+            } else {
+                res.send('Login Falhou - Usuário não cadastrado')
+            }
+        } else {
+            console.log("Erro: Consulta não realizada", err)
+            res.send('Login failed')
+        }
+    })
 })
 
 app.listen(port, () => {
